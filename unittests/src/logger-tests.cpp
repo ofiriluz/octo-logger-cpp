a8cd826380ff2a3af3afb945819afaeee4d56829
arad.yaron <arad.yaron@cyberark.com>
2024-04-15 18:09:52 +0300

    minor fix

--- unittests/src/logger-tests.cpp
+++ unittests/src/logger-tests.cpp
@@ -37,7 +37,7 @@ TEST_CASE_METHOD(LoggerTestsFixture, "Logger Initialization Tests", "[logger]")
     {
         LoggerMock logger2("basic_test_logger");
 
-        REQUIRE_THAT(&logger.channel(), &logger2.channel());
+        REQUIRE_THAT(&logger.logger_channel(), &logger2.logger_channel());
     }
 
 }
