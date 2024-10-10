1bfebdc481468856c5a38f9e311e9e517e8b184b
arad.yaron <arad.yaron@cyberark.com>
2024-10-10 16:05:22 +0300

    add test

--- unittests/include/cloudwatch-sink-mock.hpp
+++ unittests/include/cloudwatch-sink-mock.hpp
@@ -23,13 +23,17 @@ class CloudWatchSink::CloudWatchSinkMock : public CloudWatchSink
                        LogStreamType log_stream_type = DEFAULT_LOG_STREAM_TYPE,
                        bool include_date_on_log_stream = DEFAULT_INCLUDE_DATE_ON_LOG_STREAM,
                        std::string const& log_group_name = DEFAULT_LOG_GROUP_NAME,
-                       LogGroupTags log_group_tags = {})
+                       LogGroupTags log_group_tags = {},
+                       allow_overriding_by_aws_lambda_log_env = false,
+                       log_thread_id = false)
         : CloudWatchSink(config,
                          std::move(origin),
                          log_stream_type,
                          include_date_on_log_stream,
                          log_group_name,
-                         std::move(log_group_tags))
+                         std::move(log_group_tags),
+                         allow_overriding_by_aws_lambda_log_env,
+                         log_thread_id)
     {
     }
 
