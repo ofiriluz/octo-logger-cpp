d96be835e61f655852664ee4bcc28306214b7d83
Denis Sheyer <denis.sheyer@cyberark.com>
2025-01-21 09:47:23 +0200

    Wrap ConsoleJSONSink::dump with a try-catch to catch JSON related errors when dumping logs.

--- src/sinks/console-json-sink.cpp
+++ src/sinks/console-json-sink.cpp
@@ -10,6 +10,8 @@
 
 #include "octo-logger-cpp/sinks/console-json-sink.hpp"
 
+#include <nlohmann/json.hpp>
+
 namespace octo::logger
 {
 ConsoleJSONSink::ConsoleJSONSink(SinkConfig const& sink_config)
@@ -26,10 +28,20 @@ void ConsoleJSONSink::dump(Log const& log,
 {
     if (log.has_stream())
     {
-        auto log_json = construct_log_json(log, channel, context_info, global_context_info);
-        log_json["host"] = host_;
-        log_json["service"] = service_;
-        std::cout << log_json.dump() << std::endl;
+        try
+        {
+            auto log_json = construct_log_json(log, channel, context_info, global_context_info);
+            log_json["host"] = host_;
+            log_json["service"] = service_;
+            std::cout << log_json.dump() << std::endl;
+        }
+        catch (nlohmann::json::exception const& ex)
+        {
+            // Fallback to default upon exception
+            std::cerr << "Failed to dump log to console in JSON format: " << ex.what() << std::endl;
+            std::cout << formatted_log_plaintext_long(log, channel, context_info, global_context_info, false)
+                      << std::endl;
+        }
     }
 }
 
