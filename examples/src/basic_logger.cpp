47ce3d025ece74b699610554b160d0f76c600663
Arad Yaron <arad.yaron@cyberark.com>
2025-06-19 20:34:55 +0300

    revert

--- examples/src/basic_logger.cpp
+++ examples/src/basic_logger.cpp
@@ -17,8 +17,7 @@ int main(int argc, char** argv)
     auto config = std::make_shared<octo::logger::ManagerConfig>();
     config->set_option(octo::logger::ManagerConfig::LoggerOption::DEFAULT_CHANNEL_LEVEL,
                        octo::logger::Log::LogLevel::TRACE);
-    octo::logger::SinkConfig console_sink("Console", octo::logger::SinkConfig::SinkType::CONSOLE_JSON_SINK);
-    console_sink.set_option(octo::logger::SinkConfig::SinkOption::LOG_THREAD_ID, true);
+    octo::logger::SinkConfig console_sink("Console", octo::logger::SinkConfig::SinkType::CONSOLE_SINK);
     config->add_sink(console_sink);
     octo::logger::Manager::instance().configure(config);
 
