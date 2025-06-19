86a1a7708d667b3e33b17343be20f003e2a16571
Arad Yaron <arad.yaron@cyberark.com>
2025-06-19 20:36:34 +0300

    update comment

--- src/compat.cpp
+++ src/compat.cpp
@@ -1,11 +1,11 @@
 /**
- * @file channel.cpp
- * @author ofir iluz (iluzofir@gmail.com)
+ * @file compat.cpp
+ * @author Arad Yaron (aradyaron98@gmail.com)
  * @brief
  * @version 0.1
- * @date 2022-08-11
+ * @date 2025-06-19
  *
- * @copyright Copyright (c) 2022
+ * @copyright Copyright (c) 2025
  *
  */
 
