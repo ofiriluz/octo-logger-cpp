b548c54e053a546369892152781398b7948b3e59
Arad Yaron <arad.yaron@cyberark.com>
2025-06-22 20:13:24 +0300

    comment

--- src/compat.cpp
+++ src/compat.cpp
@@ -114,17 +114,8 @@ static void gmtime_safe_internal(time_t time, long timezone, struct tm *tm_time)
 } // namespace
 
 
-// Taken from here https://sourceware.org/bugzilla/show_bug.cgi?id=16145
 struct tm *gmtime_safe(time_t time, struct tm *tm_time)
 {
-    // The reason is that localtime/gmtime internally use __tz_convert, which locks.
-    // Upon fork, in rare occasions, the child process may inherit the lock,
-    // causing a deadlock if the child process tries to call localtime.
-    // To avoid this, we use an internal function that does not lock.
-    // You can find source here https://sourceware.org/bugzilla/show_bug.cgi?id=16145
-    // This is a partial implementation that matches most needs, but does not handle all use cases, 
-    // like daylight saving time, and leap seconds.
-    // That is why we use it only in case of utc, with the assumption that the timezone is intentional.
     gmtime_safe_internal(time, 0, tm_time);
     return tm_time;
 }
