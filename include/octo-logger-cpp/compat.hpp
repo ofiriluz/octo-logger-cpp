c370b170de3f35ef27f081f05256c817d32f3dc7
Arad Yaron <arad.yaron@cyberark.com>
2025-06-22 20:09:02 +0300

    move log

--- include/octo-logger-cpp/compat.hpp
+++ include/octo-logger-cpp/compat.hpp
@@ -20,6 +20,13 @@ namespace octo::logger::compat
 {
 
 // Taken from here https://sourceware.org/bugzilla/show_bug.cgi?id=16145
+// The reason is that localtime/gmtime internally use __tz_convert, which locks.
+// Upon fork, in rare occasions, the child process may inherit the lock,
+// causing a deadlock if the child process tries to call localtime.
+// To avoid this, we use an internal function that does not lock.
+// This is a partial implementation that matches most needs, but does not handle all use cases, 
+// like daylight saving time, and leap seconds.
+// That is why we use it only in case of utc, with the assumption that the timezone is intentional.
 struct tm *gmtime_safe(time_t time, struct tm *tm_time);
 
 struct tm* localtime(const time_t* timep, struct tm* result);
