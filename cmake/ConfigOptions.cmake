af5f5e84af780c5f1d680046fbdebc70596abccb
Arad Yaron <arad.yaron@cyberark.com>
2025-07-01 10:18:14 +0300

    add make config for perofmance tests

--- cmake/ConfigOptions.cmake
+++ cmake/ConfigOptions.cmake
@@ -1,3 +1,4 @@
 OPTION(DISABLE_EXAMPLES "Disable Compile examples" OFF)
 OPTION(WITH_JSON_FORMATTING "Enable JSON log formatting." OFF)
 OPTION(WITH_AWS "Enables AWS cloudwatch sink and system logger support" OFF)
+OPTION(WITH_PERFORMANCE_TESTS "Enables Performance tests" OFF)
