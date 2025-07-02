ofiriluz/octo-logger-cpp

commit ac780b2de52682f67929e95eeb9167e9ec2f94ae

Add options to avoid deadlocks upon fork (Version updated to 2.5.0) (#38)
* add step pre-fork

* lock before fork

* change names

* remove

* rename

* use ptherad_atfork where supported

* error handling

* cleanup

* add localtime_safe implementation

* move log

* comment

* use config instead

* update minor only

* cleanup

* comment

* add inline + tests

* add inline + tests

* use rand

* use rand

* missing tab

* typo

* CR suggestions

* cr suggestions

* added comments on the copy pasted implmentation

VERSION
@@ -1,1 +1,1 @@
-2.4.0
+2.5.0

src/aws/cloudwatch-sink.cpp
@@ -367,11 +367,12 @@ std::string CloudWatchSink::formatted_json(Log const& log,
     std::time_t const log_time_t = std::chrono::system_clock::to_time_t(log.time_created());
     struct tm timeinfo;
     auto const ms = std::chrono::duration_cast<std::chrono::milliseconds>(log.time_created().time_since_epoch()) % 1000;
+    compat::localtime(&log_time_t, &timeinfo, safe_localtime_utc_);
     // Put datetime with milliseconds: YYYY-MM-DDTHH:MM:SS.mmm
-    ss << std::put_time(compat::localtime(&log_time_t, &timeinfo), "%FT%T");
+    ss << std::put_time(&timeinfo, "%FT%T");
     ss << "." << std::setfill('0') << std::setw(3) << ms.count();
     // Put timezone as offset from UTC: ±HHMM
-    ss << std::put_time(compat::localtime(&log_time_t, &timeinfo), "%z");
+    ss << std::put_time(&timeinfo, "%z");
     j["message"] = log.str();
     j["origin"] = origin_;
     j["origin_service_name"] = channel.channel_name();

src/compat.cpp
@@ -1,0 +1,87 @@
+/**
+ * @file compat.cpp
+ * @author Arad Yaron (aradyaron98@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-06-19
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <cstdint>
+#include <iostream>
+#include "octo-logger-cpp/compat.hpp"
+namespace octo::logger::compat
+{
+
+// See https://sourceware.org/bugzilla/show_bug.cgi?id=16145
+void gmtime_safe_internal(time_t time, long timezone, struct tm *tm_time) 
+{
+    const char Days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    std::uint32_t n32_Pass4year = 0;
+    std::uint32_t n32_hpery = 0;
+
+    time=time + (timezone * 60 * 60);
+
+    if(time < 0)
+    {
+        time = 0;
+    }
+    tm_time->tm_sec=(int)(time % 60);
+    time /= 60;
+    tm_time->tm_min=(int)(time % 60);
+    time /= 60;
+    // Find number of 4-year periods (1461 days) passed since the epoch
+    n32_Pass4year=((unsigned int)time / (1461L * 24L));
+    // Multiply number of 4-year periods by 4 in order to get the number of years passed since the epoch, and add 70 because the epoch year is 1970
+    tm_time->tm_year=(n32_Pass4year << 2)+70;
+    time %= 1461L * 24L;
+    for (;;)
+    {
+        n32_hpery = 365 * 24;
+        // Check for Leap year
+        if ((tm_time->tm_year & 3) == 0)
+        {
+            n32_hpery += 24;
+        }
+        if (time < n32_hpery)
+        {
+            break;
+        }
+        tm_time->tm_year++;
+        time -= n32_hpery;
+    }
+    tm_time->tm_hour=(int)(time % 24);
+    // At this point `time` represents the number of days passed since the epoch
+    time /= 24;
+    // Assumed to be due to days starting from 1, so we add 1 to the day count
+    time++;
+    if ((tm_time->tm_year & 3) == 0)
+    {
+        if (time > 60)
+        {
+            // Decrement the day by 1 to account for the extra day in February, so that the day in the month is calculated correctly below
+            time--;
+        }
+        else
+        {
+            // Account for the edge case of February 29th on a leap year
+            if (time == 60)
+            {
+                tm_time->tm_mon = 1;
+                tm_time->tm_mday = 29;
+                return;
+            }
+        }
+    }
+    // Find the month and day in the month
+    for (tm_time->tm_mon = 0; Days[tm_time->tm_mon] < time;tm_time->tm_mon++)
+    {
+        time -= Days[tm_time->tm_mon];
+    }
+
+    tm_time->tm_mday = (int)(time);
+    return;
+}
+
+} // namespace octo::logger::compat

src/sink.cpp
@@ -37,7 +37,7 @@ std::string Sink::formatted_log_plaintext_long(Log const& log,
     std::time_t time = std::chrono::duration_cast<std::chrono::seconds>(ms).count();
     auto fraction = ms.count() % 1000;
     struct tm timeinfo;
-    std::strftime(dtf, sizeof(dtf), "[%d/%m/%Y %H:%M:%S", compat::localtime(&time, &timeinfo));
+    std::strftime(dtf, sizeof(dtf), "[%d/%m/%Y %H:%M:%S", compat::localtime(&time, &timeinfo, safe_localtime_utc_));
     std::string extra_id;
     if (!log.extra_identifier().empty())
     {
@@ -111,11 +111,13 @@ nlohmann::json Sink::construct_log_json(Log const& log,
     std::time_t const log_time_t = std::chrono::system_clock::to_time_t(log.time_created());
     struct tm timeinfo;
     auto const ms = std::chrono::duration_cast<std::chrono::milliseconds>(log.time_created().time_since_epoch()) % 1000;
+    compat::localtime(&log_time_t, &timeinfo, safe_localtime_utc_);
     // Put datetime with milliseconds: YYYY-MM-DDTHH:MM:SS.mmm
-    ss << std::put_time(compat::localtime(&log_time_t, &timeinfo), "%FT%T");
+    ss << std::put_time(&timeinfo, "%FT%T");
     ss << "." << std::setfill('0') << std::setw(3) << ms.count();
     // Put timezone as offset from UTC: ±HHMM
-    ss << std::put_time(compat::localtime(&log_time_t, &timeinfo), "%z");
+    ss << std::put_time(&timeinfo, "%z");
+
     j["message"] = log.str();
     j["origin"] = origin_;
     j["origin_service_name"] = channel.channel_name();
@@ -233,7 +235,8 @@ void Sink::stop(bool discard)
 }
 
 Sink::Sink(const SinkConfig& config, std::string const& origin, LineFormat format)
-    : config_(config), is_discarding_(false), origin_(origin), line_format_(format)
+    : config_(config), is_discarding_(false), origin_(origin), line_format_(format), 
+    safe_localtime_utc_(config.option_default(SinkConfig::SinkOption::USE_SAFE_LOCALTIME_UTC, false))
 {
 }
 } // namespace octo::logger

src/sinks/file-sink.cpp
@@ -49,7 +49,7 @@ void FileSink::create_log_path()
         char dtf[TIME_FORMAT_SIZE];
         std::time_t const time_v = std::time(nullptr);
         struct tm timeinfo;
-        std::strftime(dtf, sizeof(dtf), "%d-%m-%Y", compat::localtime(&time_v, &timeinfo));
+        std::strftime(dtf, sizeof(dtf), "%d-%m-%Y", compat::localtime(&time_v, &timeinfo, safe_localtime_utc_));
         log_path_ += std::string(dtf);
     }
 }
@@ -92,7 +92,7 @@ void FileSink::switch_stream(const std::string& channel)
         char dtf[TIME_FORMAT_SIZE] = {};
         std::time_t const time_v = std::time(nullptr);
         struct tm timeinfo;
-        std::strftime(dtf, sizeof(dtf), strftime_format_.c_str(), compat::localtime(&time_v, &timeinfo));
+        std::strftime(dtf, sizeof(dtf), strftime_format_.c_str(), compat::localtime(&time_v, &timeinfo, safe_localtime_utc_));
         ss << "_" << dtf;
     }
     // If Channel file exists, close it and move to the index for this channel

unittests/src/localtime-safe-tests.cpp
@@ -1,0 +1,67 @@
+#include <catch2/catch_all.hpp>
+#include "octo-logger-cpp/compat.hpp"
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+TEST_CASE("gmtime_safe matches std::gmtime", "[compat][gmtime]") {
+    using namespace std::chrono;
+    // Test a range of times, including edge cases
+    std::time_t const now = std::time(nullptr);
+    std::time_t times[] = {
+        0, // Epoch
+        now,
+        now - 86400, // 1 day ago
+        now + 86400, // 1 day ahead
+        2147483647, // Year 2038 problem boundary (on 32-bit)
+    };
+    for (std::time_t t : times) {
+        std::tm safe_tm = {};
+        std::tm* safe_ptr = octo::logger::compat::gmtime_safe(t, &safe_tm);
+        std::tm* std_ptr = std::gmtime(&t);
+        REQUIRE(safe_ptr != nullptr);
+        REQUIRE(std_ptr != nullptr);
+        // Compare all fields
+        REQUIRE(safe_tm.tm_sec == std_ptr->tm_sec);
+        REQUIRE(safe_tm.tm_min == std_ptr->tm_min);
+        REQUIRE(safe_tm.tm_hour == std_ptr->tm_hour);
+        REQUIRE(safe_tm.tm_mday == std_ptr->tm_mday);
+        REQUIRE(safe_tm.tm_mon == std_ptr->tm_mon);
+        REQUIRE(safe_tm.tm_year == std_ptr->tm_year);
+        // Note: These are not supported in the current implementation of safe gmtime
+        // REQUIRE(safe_tm.tm_wday == std_ptr->tm_wday);
+        // REQUIRE(safe_tm.tm_yday == std_ptr->tm_yday);
+        // REQUIRE(safe_tm.tm_isdst == std_ptr->tm_isdst);
+    }
+}
+
+TEST_CASE("Performance: gmtime_safe vs localtime_safe", "[compat][gmtime][localtime][performance]") {
+    constexpr int N = 1'000'000;
+    std::time_t const now = std::time(nullptr);
+    std::vector<std::time_t> times;
+    times.reserve(N);
+    for (int i = 0; i < N; ++i) {
+        times.push_back(now + rand() % now);
+    }
+    std::tm tm_buf = {};
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < N; ++i) {
+        std::tm* tm = octo::logger::compat::localtime(&times[i], &tm_buf, true);
+    }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto const gmtime_duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+
+    t1 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < N; ++i) {
+        std::tm* tm = octo::logger::compat::localtime(&times[i], &tm_buf, false);
+    }
+    t2 = std::chrono::high_resolution_clock::now();
+    auto const localtime_duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+
+    std::cout << "gmtime_safe: " << gmtime_duration << " ms, localtime_safe: " << localtime_duration << " ms" << std::endl;
+    // Not a correctness test, just for timing
+    REQUIRE(true);
+}

CMakeLists.txt
@@ -22,6 +22,7 @@ ENDIF()
 # Library definition
 ADD_LIBRARY(octo-logger-cpp STATIC
     src/channel.cpp
+    src/compat.cpp
     src/context-info.cpp
     src/log.cpp
     src/logger.cpp

include/octo-logger-cpp/compat.hpp
@@ -19,8 +19,29 @@
 namespace octo::logger::compat
 {
 
-inline struct tm* localtime(const time_t* timep, struct tm* result)
+// Taken from here https://sourceware.org/bugzilla/show_bug.cgi?id=16145
+// The reason is that localtime/gmtime internally use __tz_convert, which locks.
+// Upon fork, in rare occasions, the child process may inherit the lock,
+// causing a deadlock if the child process tries to call localtime.
+// To avoid this, we use an internal function that does not lock.
+// This is a partial implementation that matches most needs, but does not handle all use cases, 
+// like daylight saving time, and leap seconds.
+// That is why we use it only in case of utc, with the assumption that the timezone is intentional.
+void gmtime_safe_internal(time_t time, long timezone, struct tm *tm_time);
+
+inline struct tm *gmtime_safe(const time_t time, struct tm *tm_time)
 {
+    long constexpr UTC = 0;
+    gmtime_safe_internal(time, UTC, tm_time);
+    return tm_time;
+}
+
+inline struct tm* localtime(time_t const* timep, struct tm* result, bool safe_utc = false)
+{
+    if (safe_utc)
+    {
+        return gmtime_safe(*timep, result);
+    }
 #ifndef _WIN32
     return localtime_r(timep, result);
 #else

commit 402abc5c4eb987ca18ead4c6d89266be9c8edc5a

[v2.5.2] ADB-2049: Fix/timestamp in context info with USE_SAFE_LOCALTIME_UTC flag enabled no longer prints garbage + add unittests for safe localtime  + fix broken Windows Github workflow
Fixes a bug introduced in https://github.com/ofiriluz/octo-logger-cpp/pull/38.

When enabling the `USE_SAFE_LOCALTIME_UTC` flag on a Sink, the timestamp in the context-info would have an erroneous timezone.

See for example below - when USE_SAFE_LOCALTIME_UTC is disabled the timestamp is `"2025-07-01T10:13:03.599+0000"` and when it is enabled the timestamp is `"2025-07-01T10:13:20.650+40613629"` (note the erroneous `+40613629` timezone):
```
Configuring Sinks with USE_SAFE_LOCALTIME_UTC=[false]
[01/07/2025 10:13:03.599][I][ProxyMSSQLInvoker][PID(60916)][TID(140737470452608)]: Initializing [mssql] server
{"context_info":{"pid":"60916"},"host":"/pas/adb-proxy/mssql-jenkins","log_level":"INFO","message":"Initializing [mssql] server","origin":"local-task-9b25831b-fbe4-41a9-a957-d1565e7728b7","origin_func_name":"","origin_service_name":"ProxyMSSQLInvoker","service":"cloudwatch","timestamp":"2025-07-01T10:13:03.599+0000"}

Configuring Sinks with USE_SAFE_LOCALTIME_UTC=[true]
[01/07/2025 10:13:20.650][I][ProxyMSSQLInvoker][PID(61374)][TID(140737470452608)]: Initializing [mssql] server
{"context_info":{"pid":"61374"},"host":"/pas/adb-proxy/mssql-jenkins","log_level":"INFO","message":"Initializing [mssql] server","origin":"local-task-55e3fd0d-7b59-4379-a344-af82827234e4","origin_func_name":"","origin_service_name":"ProxyMSSQLInvoker","service":"cloudwatch","timestamp":"2025-07-01T10:13:20.650+40613629"}                                                
```

This is probably due to the `struct tm` containing garbage data (since it was not initialized to 0 values) in fields that are not set by the `gmtime_safe_internal` function.

.github/workflows/windows.yml
@@ -10,7 +10,7 @@ jobs:
     runs-on: ${{matrix.os}}
     strategy:
       matrix:
-        os: [windows-2019]
+        os: [windows-2025]
         platform: [Win32, x64]
         toolset: [v142]
         build_type: [Debug, Release]
@@ -33,7 +33,7 @@ jobs:
       run: |
         conan profile new default --detect
         conan profile update settings.build_type=${{matrix.build_type}} default
-        conan install $GITHUB_WORKSPACE
+        conan install $GITHUB_WORKSPACE --build=catch2 --build=fmt
     - name: Build and test conan
       working-directory: ${{runner.workspace}}\\build
       shell: bash

VERSION
@@ -1,1 +1,1 @@
-2.5.1
+2.5.2

src/aws/cloudwatch-sink.cpp
@@ -365,7 +365,7 @@ std::string CloudWatchSink::formatted_json(Log const& log,
     nlohmann::json j;
     std::stringstream ss;
     std::time_t const log_time_t = std::chrono::system_clock::to_time_t(log.time_created());
-    struct tm timeinfo;
+    struct tm timeinfo = {};
     auto const ms = std::chrono::duration_cast<std::chrono::milliseconds>(log.time_created().time_since_epoch()) % 1000;
     compat::localtime(&log_time_t, &timeinfo, safe_localtime_utc_);
     // Put datetime with milliseconds: YYYY-MM-DDTHH:MM:SS.mmm

src/compat.cpp
@@ -81,6 +81,9 @@ void gmtime_safe_internal(time_t time, long timezone, struct tm *tm_time)
     }
 
     tm_time->tm_mday = (int)(time);
+
+    // Always set daylight saving time to 0 (i.e DST not in effect), as we currently do not handle it.
+    tm_time->tm_isdst = 0;
     return;
 }
 

src/sink.cpp
@@ -34,9 +34,9 @@ std::string Sink::formatted_log_plaintext_long(Log const& log,
     char dtf[1024];
     std::stringstream ss;
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(log.time_created().time_since_epoch());
-    std::time_t time = std::chrono::duration_cast<std::chrono::seconds>(ms).count();
-    auto fraction = ms.count() % 1000;
-    struct tm timeinfo;
+    std::time_t const time = std::chrono::duration_cast<std::chrono::seconds>(ms).count();
+    auto const fraction = ms.count() % 1000;
+    struct tm timeinfo = {};
     std::strftime(dtf, sizeof(dtf), "[%d/%m/%Y %H:%M:%S", compat::localtime(&time, &timeinfo, safe_localtime_utc_));
     std::string extra_id;
     if (!log.extra_identifier().empty())
@@ -109,7 +109,7 @@ nlohmann::json Sink::construct_log_json(Log const& log,
     nlohmann::json j;
     std::stringstream ss;
     std::time_t const log_time_t = std::chrono::system_clock::to_time_t(log.time_created());
-    struct tm timeinfo;
+    struct tm timeinfo = {};
     auto const ms = std::chrono::duration_cast<std::chrono::milliseconds>(log.time_created().time_since_epoch()) % 1000;
     compat::localtime(&log_time_t, &timeinfo, safe_localtime_utc_);
     // Put datetime with milliseconds: YYYY-MM-DDTHH:MM:SS.mmm

src/sinks/file-sink.cpp
@@ -48,7 +48,7 @@ void FileSink::create_log_path()
     {
         char dtf[TIME_FORMAT_SIZE];
         std::time_t const time_v = std::time(nullptr);
-        struct tm timeinfo;
+        struct tm timeinfo = {};
         std::strftime(dtf, sizeof(dtf), "%d-%m-%Y", compat::localtime(&time_v, &timeinfo, safe_localtime_utc_));
         log_path_ += std::string(dtf);
     }
@@ -91,7 +91,7 @@ void FileSink::switch_stream(const std::string& channel)
     {
         char dtf[TIME_FORMAT_SIZE] = {};
         std::time_t const time_v = std::time(nullptr);
-        struct tm timeinfo;
+        struct tm timeinfo = {};
         std::strftime(dtf, sizeof(dtf), strftime_format_.c_str(), compat::localtime(&time_v, &timeinfo, safe_localtime_utc_));
         ss << "_" << dtf;
     }

unittests/src/localtime-safe-tests.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <chrono>
+#include <iomanip>
 #include <thread>
 #include <iostream>
 
@@ -37,6 +38,72 @@ TEST_CASE("gmtime_safe matches std::gmtime", "[compat][gmtime]") {
     }
 }
 
+// Helper function to format time as ISO 8601 with milliseconds and timezone offset
+// Should be same as how we serialize the timestamp for our Cloudwatch Sink and for the Console JSON Sink
+std::string format_time(std::tm const* timeinfo) {
+    std::stringstream ss;
+    // Put datetime with milliseconds: YYYY-MM-DDTHH:MM:SS.mmm
+    ss << std::put_time(timeinfo, "%FT%T");
+    std::chrono::milliseconds const ms{123}; // Arbitrary milliseconds
+    ss << "." << std::setfill('0') << std::setw(3) << ms.count();
+    // Put timezone as offset from UTC: ±HHMM
+    ss << std::put_time(timeinfo, "%z");
+    return ss.str();
+}
+
+// Fixture to set the timezone to UTC for testing
+// Resets the original timezone after the test
+struct SetUTCTimezoneFixture {
+    std::string old_tz;
+
+    SetUTCTimezoneFixture() {
+        const char* tz = getenv("TZ");
+        old_tz = tz ? tz : "";
+
+        std::cout << "Setting timezone to UTC for tests" << std::endl;
+        setenv("TZ", "UTC", 1);
+        tzset();
+    }
+
+    ~SetUTCTimezoneFixture() {
+        if (!old_tz.empty()) {
+            std::cout << "Resetting timezone to " << old_tz << std::endl;
+            setenv("TZ", old_tz.c_str(), 1);
+        } else {
+            std::cout << "Unsetting TZ env var" << std::endl;
+            unsetenv("TZ");
+        }
+        tzset();
+    }
+};
+
+TEST_CASE_METHOD(SetUTCTimezoneFixture, "Stringify compat::localtime with std::put_time with/without safe_utc enabled should match", "[compat][gmtime][put_time]") {
+    using namespace std::chrono;
+    // Test a range of times, including edge cases
+    std::time_t const now = std::time(nullptr);
+    std::time_t times[] = {
+        0, // Epoch
+        now,
+        now - 86400, // 1 day ago
+        now + 86400, // 1 day ahead
+        2147483647, // Year 2038 problem boundary (on 32-bit)
+    };
+    for (std::time_t t : times) {
+        std::tm safe_localtime_tm = {};
+        std::tm const* safe_localtime_ptr = octo::logger::compat::localtime(&t, &safe_localtime_tm, true);
+        std::tm unsafe_localtime_tm = {};
+        std::tm const* unsafe_localtime_ptr = octo::logger::compat::localtime(&t, &unsafe_localtime_tm, false);
+        REQUIRE(safe_localtime_ptr != nullptr);
+        REQUIRE(unsafe_localtime_ptr != nullptr);
+
+        // Compare stringified times
+        std::string const safe_localtime_str = format_time(safe_localtime_ptr);
+        std::string const unsafe_localtime_str = format_time(unsafe_localtime_ptr);
+        std::cout << "Safe time: " << safe_localtime_str << ", Std time: " << unsafe_localtime_str << std::endl;
+        REQUIRE(safe_localtime_str == unsafe_localtime_str);
+    }
+}
+
 TEST_CASE("Performance: gmtime_safe vs localtime_safe", "[compat][gmtime][localtime][performance]") {
     constexpr int N = 1'000'000;
     std::time_t const now = std::time(nullptr);
@@ -65,3 +132,4 @@ TEST_CASE("Performance: gmtime_safe vs localtime_safe", "[compat][gmtime][localt
     // Not a correctness test, just for timing
     REQUIRE(true);
 }
+